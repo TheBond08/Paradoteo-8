a631c716fc858aff1aa1416f687e3a7264661053
TheBond08 <101583061+TheBond08@users.noreply.github.com>
2022-11-27 23:26:07 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,21 +5,17 @@ echo "Please choose the markdown(.md) file you wish to convert."
 read file
 
 echo "Select which of the following types you wish the .md to be converted into:
-a) .pdf
-b) .docx
-c) .html
-d) .txt"
+a) .docx
+b) .html
+c) .txt"
 read type
 
 echo "The conversion shall begin."
 
 if [ "$type"="a" ];
 then
-     pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="DejaVuSansMono" --variable sansfont="DejaVuSansMono" --variable monofont="DejaVuSansMono" --variable fontsize=12pt --variable version=2.0 $file.md  --pdf-engine=xelatex --toc -o %file.pdf
-elif [ "$type" = "b" ];
-then
     pandoc -o $file.docx -f markdown -t docx $file.md
-elif [ "$type" = "c" ];
+elif [ "$type" = "b" ];
 then
     pandoc -s $file.md --metadata title="$file" -o $file.html
 else
