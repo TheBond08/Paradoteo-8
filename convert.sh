f4217fe3f3b2a2c437f16493ba27db2ffd85f300
TheBond08 <101583061+TheBond08@users.noreply.github.com>
2022-11-27 23:10:35 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,21 +5,21 @@ echo "Please choose the markdown(.md) file you wish to convert."
 read file
 
 echo "Select which of the following types you wish the .md to be converted into:
-1) .pdf
-2) .docx
-3) .html
-4) .txt"
+a) .pdf
+b) .docx
+c) .html
+d) .txt"
 read type
 
 echo "The conversion shall begin."
 
-if [ "$type"="1" ];
+if [ "$type"="a" ];
 then
     pandoc -s -o $file.pdf $file.md
-elif [ "$type" = "2" ];
+elif [ "$type" = "b" ];
 then
     pandoc -o $file.docx -f markdown -t docx $file.md
-elif [ "$type" = "3" ];
+elif [ "$type" = "c" ];
 then
     pandoc -s $file.md --metadata title="$file" -o $file.html
 else
