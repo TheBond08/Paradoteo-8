d33e56102cfded15de7c4b79929caf888e13ec7d
TheBond08 <bontislefteris@gmail.com>
2022-11-28 08:56:32 +0000

    Conversion Completed

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #Author: inf2021160
 
-echo "Please choose the markdown(.md) file you wish to convert."
+echo "Please type name of the markdown(.md) file you wish to convert."
 
 read file
 
@@ -12,10 +12,10 @@ read type
 
 echo "The conversion shall begin."
 
-if [ "$type"="a" ];
+if [ $type = "a" ];
 then
     pandoc -o $file.docx -f markdown -t docx $file.md
-elif [ "$type" = "b" ];
+elif [ $type = "b" ];
 then
     pandoc -s $file.md --metadata title="$file" -o $file.html
 else
